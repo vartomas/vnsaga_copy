08a6086bba4645bbf89d07f69eff37d73f838ca2
vartomas <tomasvareika85@gmail.com>
2020-11-28 22:43:30 +0200

    3

--- js/header.js
+++ js/header.js
@@ -1,16 +1,35 @@
 const desktopHeaderLogo = document.querySelector('.logo')
 const desktopNav = document.querySelector('.menu')
 const mobileNav = document.querySelector('.mobileNav')
+const desktopHeaderLogoText = document.querySelector('#headerLogo')
+const header = document.querySelector('header')
+const hamburger = document.querySelector('.hamburger')
+const mobileHam = document.querySelector('.mobileHam')
 
 window.addEventListener('scroll', () => {
     const scroll = window.scrollY
     if (scroll > 20) {
-        desktopHeaderLogo.style.display = 'none'
-        desktopNav.style.display = 'none'
-        mobileNav.style.display = 'flex'
+        desktopHeaderLogo.style.width = '80px'
+        desktopHeaderLogoText.style.display = 'none'
+        mobileNav.style.visibility = 'visible'
+        desktopNav.style.transform = 'scaleX(0)'
+        header.style.width = '160px'
+        hamburger.style.transform = 'scaleX(1)'
     } else {
-        desktopHeaderLogo.style.display = 'flex'
-        desktopNav.style.display = 'block'
-        mobileNav.style.display = 'none'
+        desktopHeaderLogo.style.width = '240px'
+        desktopHeaderLogoText.style.display = 'inline-block'
+        mobileNav.style.visibility = 'hidden'
+        desktopNav.style.transform = 'scaleX(1)'
+        header.style.width = '100%'
+        hamburger.style.transform = 'scaleX(0.5)'
     }
+})
+
+mobileHam.addEventListener('click', () => {
+    desktopHeaderLogo.style.width = '80px'
+    desktopHeaderLogoText.style.display = 'none'
+    mobileNav.style.visibility = 'visible'
+    desktopNav.style.transform = 'scaleX(0)'
+    header.style.width = '160px'
+    hamburger.style.transform = 'scaleX(1)'
 })
